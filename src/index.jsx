e1c1b072deef100ad911111b36cb2b89a7c5a80e
Tom Mauboussin <tom.mauboussin@icloud.com>
2023-04-12 16:42:00 +0200

    Création du router et ajout des packages nécessaires pour l'appplication

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './index.css';
+import Header from './components/Header';
+import Footer from './components/Footer'
+import Error from './components/Error';
+import Home from './pages/Home';
+import FicheLogement from './pages/FicheLogement';
+import About from './pages/About';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/logements/:numeroLogement" element={<FicheLogement />} />
+        <Route exact path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
+);
